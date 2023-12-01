cb8b78d4586f219ddf1049bdb3986eff68386787
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2023-12-01 09:42:24 +0300

    feat(user-input): retrieve & validate user input

--- hangman/__main__.py
+++ hangman/__main__.py
@@ -4,9 +4,6 @@ Entry point of the game
 import argparse
 import sys
 
-from golife import patterns, views
-from golife.cli import get_command_line_args
-
 
 def main() -> None:
     """Entry point of the game. Gets the command line arguments and shows either all patterns of the game or a single
@@ -22,9 +19,9 @@ def main() -> None:
 
 
 def _show_pattern(
-    view: views.CursesView,
-    pattern: patterns.Pattern,
-    args: argparse.Namespace,
+        view: views.CursesView,
+        pattern: patterns.Pattern,
+        args: argparse.Namespace,
 ) -> None:
     """Shows the pattern given the view, pattern and arguments from the command line"""
     try:
