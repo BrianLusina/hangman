ddcc2ae92d3c8e8102ee2416d6616eccc8126e38
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2023-12-01 09:54:42 +0300

    feat(utils): build guessed word

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,5 @@
 import unittest
-from hangman.utils import join_guessed_letters
+from hangman.utils import join_guessed_letters, build_guessed_word
 
 
 class UtilsTestCase(unittest.TestCase):
@@ -7,6 +7,13 @@ class UtilsTestCase(unittest.TestCase):
         letters = {"a", "b", "c", "d"}
         expected = 'a b c d'
         actual = join_guessed_letters(letters)
+        self.assertEqual(expected, actual)
+
+    def test_build_guessed_word(self):
+        guessed_letters = {"a", "b", "c", "d", "p", "o"}
+        target_word = "airport"
+        expected = 'a _ _ p o _ _'
+        actual = build_guessed_word(target_word, guessed_letters)
         self.assertEqual(expected, actual)
 
 
